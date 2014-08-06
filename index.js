79ab4ed5d1b879090ad94e34cc3de8aee1a83a04
Prashanth.Kumar <Prashanth.k@SWDSK0004.(none)>
2014-08-06 16:36:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+var http = require('http'), app = exports = module.exports = {}, webstack = [], url = require('url'), parsedUrl, path, qString, method, reqHandler, foundHandler;
+
+app.start = function(port, callback) {
+	if (callback) {
+		if ( typeof callback !== 'function') {
+			throw Error('function Expected but find Something Else')
+		}
+	}
+	http.createServer(function(req, res) {
+		app.req = req;
+		app.res = res;
+		app.handle(req, res);
+	}).listen(port, callback());
+};
+
+app.get = function(path, callback) {
+	webstack.push({
+		'path' : path,
+		'callback' : callback,
+		'method' : 'GET'
+	});
+};
+
+app.put = function(path, callback) {
+	webstack.push({
+		'path' : path,
+		'callback' : callback,
+		'method' : 'PUT'
+	});
+};
+
+app.post = function(path, callback) {
+	webstack.push({
+		'path' : path,
+		'callback' : callback,
+		'method' : 'POST'
+	});
+};
+
+app.delete = function(path, callback) {
+	webstack.push({
+		'path' : path,
+		'callback' : callback,
+		'method' : 'DELETE'
+	});
+};
+
+app.handle = function(req, res) {
+	parsedUrl = url.parse(req.url, true);
+	path = parsedUrl.pathname;
+	qString = parsedUrl.query;
+	method = req.method;
+	reqHandler = {};
+	foundHandler = false;
+	for (var i = 0; i < webstack.length; i++) {
+		if (webstack[i].path === path && webstack[i].method === method) {
+			reqHandler.handle = webstack[i].callback;
+			foundHandler = true;
+			break;
+		}
+	}
+	if (foundHandler) {
+		reqHandler.handle(req, res);
+	} else {
+		res.end("Can't " + method + ' path');
+	}
+};
