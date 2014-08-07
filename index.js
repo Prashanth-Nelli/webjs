1623f502e79e64086c70e6b552b9613be6596236
Prashanth.Kumar <Prashanth.k@SWDSK0004.(none)>
2014-08-07 10:39:43 +0530

    update

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-var http = require('http'), app = exports = module.exports = {}, webstack = [], url = require('url'), parsedUrl, path, qString, method, reqHandler, foundHandler;
-var str='',parser=require('./parser');
+var http = require('http');
+var app = exports = module.exports = {};
+var webstack = [];
+var url = require('url');
+var parser = require('./parser');
+var querystring = require('querystring');
+
 app.start = function(port, callback) {
 	if (callback) {
 		if ( typeof callback !== 'function') {
 			throw Error('function Expected but find Something Else')
 		}
+	} else {
+		callback = function() {
+
+		};
 	}
 	http.createServer(function(req, res) {
 		app.req = req;
 		app.res = res;
-		str='';
-		req.on('data',function(chunck){
-			str+=chunck;
-			app.handle(req, res,str);
-		});		
+		str = '';
+		req.on('data', function(chunck) {
+			str += chunck;
+			if (str.length > 1e6) {
+				response.writeHead(413, {
+					'Content-Type' : 'text/plain'
+				}).end();
+				req.connection.destroy();
+			}
+		});
+		req.on('end', function() {
+			app.handle(req, res, str);
+		})
 	}).listen(port, callback());
 };
 
 app.get = function(path, callback) {
 	webstack.push({
-		'path' : path,
+		'path' : pathparse(path).path,
+		'actualpath' : path,
 		'callback' : callback,
 		'method' : 'GET'
 	});
@@ -27,7 +45,8 @@ app.get = function(path, callback) {
 
 app.put = function(path, callback) {
 	webstack.push({
-		'path' : path,
+		'path' : pathparse(path).path,
+		'actualpath' : path,
 		'callback' : callback,
 		'method' : 'PUT'
 	});
@@ -35,7 +54,8 @@ app.put = function(path, callback) {
 
 app.post = function(path, callback) {
 	webstack.push({
-		'path' : path,
+		'path' : pathparse(path).path,
+		'actualpath' : path,
 		'callback' : callback,
 		'method' : 'POST'
 	});
@@ -43,30 +63,65 @@ app.post = function(path, callback) {
 
 app.delete = function(path, callback) {
 	webstack.push({
-		'path' : path,
+		'path' : pathparse(path).pathname,
+		'actualpath' : path,
 		'callback' : callback,
 		'method' : 'DELETE'
 	});
 };
 
-app.handle = function(req, res,data) {
-	parsedUrl = url.parse(req.url, true);
-	path = parsedUrl.pathname;
-	qString = parsedUrl.query;
-	method = req.method;
-	reqHandler = {};
-	foundHandler = false;
+app.handle = function(req, res, data) {
+
+	var path = pathparse(req.url).path;
+	var qString = pathparse(req.url).query;
+	var method = req.method;
+	var reqHandler = {};
+	var foundHandler = false;
+	var swPath = '';
+	var sPath = '';
+
 	for (var i = 0; i < webstack.length; i++) {
 		if (webstack[i].path === path && webstack[i].method === method) {
+
 			reqHandler.handle = webstack[i].callback;
 			foundHandler = true;
 			break;
+
+		} else if (webstack[i].method === method) {
+
+			swPath = webstack[i].path;
+			swPath = swPath.slice(0, swPath.lastIndexOf('/'));
+			sPath = path.slice(0, path.lastIndexOf('/'));
+			if (swPath === sPath) {
+				swPath = webstack[i].path;
+				req.params = {};
+				req.params[swPath.slice(swPath.lastIndexOf('/') + 2, swPath.length)] = path.slice(path.lastIndexOf('/') + 1, path.length);
+				reqHandler.handle = webstack[i].callback;
+				foundHandler = true;
+				break;
+
+			}
 		}
 	}
-	req['body']=parser.parse(data,req.headers['content-type']);
+
+	req['body'] = parser.parse(data, req.headers['content-type']);
+
+	if (qString) {
+		req['query'] = qString;
+	}
 	if (foundHandler) {
 		reqHandler.handle(req, res);
 	} else {
 		res.end("Can't " + method + ' path');
 	}
 };
+
+function pathparse(path) {
+	var obj = {};
+	obj.path = url.parse(path, true).pathname;
+	if (obj.path.lastIndexOf('/') === obj.path.length - 1) {
+		obj.path = obj.path.slice(0, obj.path.length - 1);
+	}
+	obj.query = url.parse(path, true).query;
+	return obj;
+}
--- parser.js
+++ parser.js
@@ -1,22 +1,17 @@
-var parser = exports = module.exports={}
-	,resObj={}
-	,length=0
-	,keyValue='';
+var parser = exports = module.exports={};
+var querystring=require('querystring');
+
+
 parser.parse=function(data,encoding){
 	switch(encoding){
 		case 'application/json':
 			return JSON.parse(data);
 		break;
 		case 'application/x-www-form-urlencoded':
-		length=data.split('&').length;
-		data=data.split('&');
-		for(var i=0;i<length;i++){
-			keyValue=data[i].split('=');
-			resObj[keyValue[0]]=keyValue[1];
-		}
-		return resObj;
+		return querystring.parse(data);
 		break;
 		defalut:
+		console.log('');
 	}	
 };
 
